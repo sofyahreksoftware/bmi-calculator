5bb3a141f092c39ddbd60e70b06a661132bed1d1
sofyahrek <sofigrekal@gmail.com>
2024-01-09 14:16:02 +0100

    chore: form button transition with scale,  js code in curly braces

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-const displayResult = (result, category) => {
-  const resultLabel = document.querySelector(".js-resultLabel");
+{
+  const displayResult = (result, category) => {
+    const resultLabel = document.querySelector(".js-resultLabel");
 
-  resultLabel.innerHTML = `
+    resultLabel.innerHTML = `
       <p>Your BMI is 
       <strong>${Number(result.toFixed(2)).toLocaleString("pl-PL")}</strong>.
       </p>
@@ -12,50 +13,51 @@ const displayResult = (result, category) => {
           ? `You are <strong>${category}</strong>.`
           : `You have an <strong>${category}</strong>.`
       }</p>`;
-};
+  };
 
-const switchBMICategory = (result) => {
-  let category;
+  const switchBMICategory = (result) => {
+    let category;
 
-  switch (true) {
-    case result < 18.5:
-      category = "underweight";
-      break;
-    case result >= 18.5 && result < 24.9:
-      category = "normal weight";
-      break;
-    case result >= 24.9 && result < 29.9:
-      category = "overweight";
-      break;
-      D;
-    case result >= 29.9 && result < 34.9:
-      category = "obesity class I (Moderate obesity)";
-      break;
-    case result >= 34.9 && result < 39.9:
-      category = "obesity class II (Severe obesity)";
-      break;
-    case result >= 39.9:
-      category = "obesity class III (Very severe obesity)";
-      break;
+    switch (true) {
+      case result < 18.5:
+        category = "underweight";
+        break;
+      case result >= 18.5 && result < 24.9:
+        category = "normal weight";
+        break;
+      case result >= 24.9 && result < 29.9:
+        category = "overweight";
+        break;
+        D;
+      case result >= 29.9 && result < 34.9:
+        category = "obesity class I (Moderate obesity)";
+        break;
+      case result >= 34.9 && result < 39.9:
+        category = "obesity class II (Severe obesity)";
+        break;
+      case result >= 39.9:
+        category = "obesity class III (Very severe obesity)";
+        break;
 
-    default:
-      category = "indefinite category";
-  }
+      default:
+        category = "indefinite category";
+    }
 
-  displayResult(result, category);
-};
+    displayResult(result, category);
+  };
 
-const calculateResult = () => {
-  const form = document.querySelector(".js-form");
-  const weight = document.querySelector(".js-weight");
-  const height = document.querySelector(".js-height");
+  const calculateResult = () => {
+    const form = document.querySelector(".js-form");
+    const weight = document.querySelector(".js-weight");
+    const height = document.querySelector(".js-height");
 
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let result;
-    result = weight.value / height.value ** 2;
-    switchBMICategory(result);
-  });
-};
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      let result;
+      result = weight.value / height.value ** 2;
+      switchBMICategory(result);
+    });
+  };
 
-calculateResult();
+  calculateResult();
+}
